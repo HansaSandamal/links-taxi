5893b82c1987b68438d4b814a0f2bc9292015a80
Hansa Sandamal <48385589+HansaSandamal@users.noreply.github.com>
2024-01-07 11:07:45 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Links Taxi</h1>
+      <p>Services: Airport Transfers, Long Distance, Day Tours, City Tours, Yala Udawalawa Safari</p>
+      <p>Contact Details: 0777598000, 0713366180</p>
+      <p>Email: linkstourssrilanka@gmail.com</p>
     </div>
   );
 }
